bc8c9f9781b1e412c074b3a5dc92fe2b8e5de607
saeipi <saeipi@163.com>
2020-05-05 01:33:40 +0800

    FFmpeg

---
+++ KSPlayer/Scripts/Custom/FFmpeg/Decoder/KSVideoDecoder.h
@@ -0,0 +1,17 @@
+//
+//  KSVideoDecoder.h
+//  KSPlayer
+//
+//  Created by Athena on 2020/5/5.
+//  Copyright © 2020 Athena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KSVideoDecoder : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KSPlayer/Scripts/Custom/FFmpeg/Decoder/KSVideoDecoder.m
@@ -0,0 +1,13 @@
+//
+//  KSVideoDecoder.m
+//  KSPlayer
+//
+//  Created by Athena on 2020/5/5.
+//  Copyright © 2020 Athena. All rights reserved.
+//
+
+#import "KSVideoDecoder.h"
+
+@implementation KSVideoDecoder
+
+@end
