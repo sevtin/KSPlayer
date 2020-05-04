3759622439b8abac42f599326983dcd5d64cf722
huchaojun <huchaojun@izunyou.com>
2020-05-05 00:51:07 +0800

    项目初始化

---
+++ KSPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KSPlayer
+//
+//  Created by saeipi on 2020/5/5.
+//  Copyright © 2020 saeipi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KSPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KSPlayer
+//
+//  Created by saeipi on 2020/5/5.
+//  Copyright © 2020 saeipi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
