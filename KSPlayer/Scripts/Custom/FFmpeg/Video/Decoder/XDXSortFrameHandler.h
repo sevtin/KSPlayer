1144d99b799c9d77590040a947270ce34645bc63
saeipi <saeipi@163.com>
2020-05-05 21:49:10 +0800

    XDXSortFrameHandler 格式化

--- KSPlayer/Scripts/Custom/FFmpeg/Video/Decoder/XDXSortFrameHandler.h
+++ KSPlayer/Scripts/Custom/FFmpeg/Video/Decoder/XDXSortFrameHandler.h
@@ -9,22 +9,19 @@
 #import <Foundation/Foundation.h>
 #import "XDXAVParseHandler.h"
 
-NS_ASSUME_NONNULL_BEGIN
-
 @protocol XDXSortFrameHandlerDelegate <NSObject>
 
 @optional
+
 - (void)getSortedVideoNode:(CMSampleBufferRef)videoDataRef;
 
 @end
 
 @interface XDXSortFrameHandler : NSObject
 
-@property (weak  , nonatomic) id<XDXSortFrameHandlerDelegate> delegate;
+@property (nonatomic, weak) id<XDXSortFrameHandlerDelegate> delegate;
 
 - (void)addDataToLinkList:(CMSampleBufferRef)videoDataRef;
 - (void)cleanLinkList;
 
 @end
-
-NS_ASSUME_NONNULL_END
